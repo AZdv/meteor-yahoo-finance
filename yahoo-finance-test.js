955cef961c55055b47eb8abaa4bcc7e0fb9c95a4
Asaf Zamir <asaf.zamir@gmail.com>
2015-04-22 22:29:33 +0200

    Updating tests for NPM package yahoo-finance version 0.2.11

--- yahoo-finance-test.js
+++ yahoo-finance-test.js
@@ -4,7 +4,7 @@ Tinytest.add('YahooFinance - Historical Function Export', function(test) {
 
 Tinytest.add('YahooFinance - Historical Quote (single)', function(test) {
     var symbol = 'YHOO';
-    var testDate = '2014-06-02T07:00:00.000Z';
+    var testDate = new Date('2014-06-01T22:00:00.000Z');
     var expectedQuote = {
       symbol: 'YHOO',
       open: 34.69,
@@ -13,7 +13,7 @@ Tinytest.add('YahooFinance - Historical Quote (single)', function(test) {
       close: 34.87,
       volume: 9178900,
       adjClose: 34.87,
-      date: new Date(testDate)
+      date: testDate
     }
 
     var quotes = YahooFinance.historical({symbol:symbol, from:testDate, to:testDate});
@@ -26,44 +26,44 @@ Tinytest.add('YahooFinance - Historical Quote (single)', function(test) {
 Tinytest.add('YahooFinance - Historical Quote (multiple)', function(test) {
     var symbols = ['AAPL','GOOGL','YHOO'];
     var fields = ['n','s','p'];
-    var testDate = new Date('2014-06-02T07:00:00.000Z');
-    var expectedQuotes = [{
-      symbol: 'AAPL',
-      open: 633.96,
-      high: 634.83,
-      low: 622.5,
-      close: 628.65,
-      volume: 92337700,
-      adjClose: 89.36,
-      date: testDate
-    },{
-      symbol: 'GOOGL',
-      open: 569.75,
-      high: 570.41,
-      low: 556.7,
-      close: 564.34,
-      volume: 1660500,
-      adjClose: 564.34,
-      date: testDate
-    },{
-      symbol: 'YHOO',
-      open: 34.69,
-      high: 34.95,
-      low: 34.28,
-      close: 34.87,
-      volume: 9178900,
-      adjClose: 34.87,
-      date: testDate
-    }];
+    var testDate = new Date('2014-06-01T22:00:00.000Z');
+    var expectedQuotes =  {
+        AAPL: [{ 
+            date: testDate,
+            open: 633.95996,
+            high: 634.83002,
+            low: 622.5,
+            close: 628.64996,
+            volume: 92337700,
+            adjClose: 88.62704,
+            symbol: 'AAPL' } ],
+       GOOGL: [{
+            date: testDate,
+            open: 569.75,
+            high: 570.40997,
+            low: 556.70001,
+            close: 564.34003,
+            volume: 1660500,
+            adjClose: 564.34003,
+            symbol: 'GOOGL' } ],
+       YHOO: [{
+            date: testDate,
+            open: 34.69,
+            high: 34.95,
+            low: 34.28,
+            close: 34.87,
+            volume: 9178900,
+            adjClose: 34.87,
+            symbol: 'YHOO'
+        }]
+    }
+
 
     var stocks = YahooFinance.historical({symbols:symbols, from:testDate, to:testDate});
-    // console.log(stocks[0].quotes[0]);
+    // console.log(stocks);
 
-    test.instanceOf(stocks, Array);
-
-    test.equal(stocks[0].quotes[0], expectedQuotes[0]);
-    test.equal(stocks[1].quotes[0], expectedQuotes[1]);
-    test.equal(stocks[2].quotes[0], expectedQuotes[2]);
+    test.instanceOf(stocks, Object);
+    test.equal(stocks, expectedQuotes);
 });
 
 
@@ -74,14 +74,15 @@ Tinytest.add('YahooFinance - Snapshot Function Export', function(test) {
 Tinytest.add('YahooFinance - Snapshot Quote', function(test) {
     var symbols = ['AAPL','GOOGL','YHOO'];
     var fields = ['s','n'];
-    var expectedSnapshots = {
-      AAPL: {name:'Apple Inc.', symbol:'AAPL'},
-      GOOGL: {name:'Google Inc.', symbol:'GOOGL'},
-      YHOO: {name:'Yahoo! Inc.', symbol:'YHOO'}
-    };
+    var expectedSnapshots = [
+      {name:'Apple Inc.', symbol:'AAPL'},
+      {name:'Google Inc.', symbol:'GOOGL'},
+      {name:'Yahoo! Inc.', symbol:'YHOO'}
+    ];
 
     var snapshots = YahooFinance.snapshot({symbols:symbols, fields:fields});
     // console.log(snapshots);
 
+    test.instanceOf(snapshots, Array);
     test.equal(snapshots, expectedSnapshots);
 });
